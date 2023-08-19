2f9082b503b98ed7bd5e40893fef7f4ee649d166
Phahladira-Moja <phahladira@patala.co.za>
2023-08-20 01:03:49 +0200

    Completed/Added the navbar design

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,33 @@
+import "./Navbar.scss";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="/logo.svg" alt="" className="icon" />
+        <span>lamadmin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+
+        <div className="notification">
+          <img src="/notifications.svg" alt="" className="icon" />
+          <span>1</span>
+        </div>
+
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+          />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
