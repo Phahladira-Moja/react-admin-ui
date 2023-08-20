f434042dfde4cde75653830b9724d9891ad45fc7
Phahladira-Moja <phahladira@patala.co.za>
2023-08-20 15:01:49 +0200

    Added the data table materia ui package
    - added the data table to the users page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,68 @@
+import DataTable from "../../components/dataTable/DataTable";
 import "./Users.scss";
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { userRows } from "../../utils/data";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "./noavatar.png"} alt="" />;
+    },
+  },
+
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    headerName: "Phone Number",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
 
 const Users = () => {
-  return <div className="users">Users</div>;
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+
+      <DataTable slug="users" rows={userRows} columns={columns} />
+    </div>
+  );
 };
 
 export default Users;
